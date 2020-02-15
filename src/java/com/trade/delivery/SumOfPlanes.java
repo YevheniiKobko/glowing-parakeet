81a564fe20240cc94cba6b6fd647ff5ed8f72d5a
YevheniiKobko <zhenyakobkoiua@gmail.com>
2020-02-15 02:45:27 +0200

    Minor bug fixes

--- src/java/com/trade/delivery/SumOfPlanes.java
+++ src/java/com/trade/delivery/SumOfPlanes.java
@@ -4,24 +4,21 @@ import java.util.Scanner;
 
 public class SumOfPlanes {
     public static void main(String[] args) {
-        System.out.println("Your number: "+ sum());
-
+        System.out.println("Your number: " + sum());
     }
-    /* Creating a variable int sum. It calculates the sum of two numbers*/
+    //Creating a variable int sum. It calculates the sum of two numbers
     public static int sum () {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the first number: ");
         int a = scan.nextInt();
         System.out.println("Enter the second number: ");
         int b = scan.nextInt();
-        int sum = a+b;
+        int sum = a + b;
         return sum;
     }
-    /*Created a new method that takes two parameters at the input*/
+    //Created a new method that takes two parameters at the input
     public Integer sumOfNumbers (int firstNumbers, int secondNumbers){
         int sum = firstNumbers + secondNumbers;
         return sum;
     }
-
-
-    }
+}
