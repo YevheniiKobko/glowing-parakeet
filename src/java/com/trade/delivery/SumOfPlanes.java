393a30385776019efd9d09cbb1a8439eabcfaf0a
YevheniiKobko <zhenyakobkoiua@gmail.com>
2020-02-17 18:16:58 +0200

    Fixed a bug with the return value

--- src/java/com/trade/delivery/SumOfPlanes.java
+++ src/java/com/trade/delivery/SumOfPlanes.java
@@ -1,14 +1,14 @@
 package com.trade.delivery;
 import java.util.Scanner;
 public class SumOfPlanes {
-    // Cause method sumOfNumbers.
+    // Cause method sumOfNumbers
     public static void main(String[] args) {
         int d = sumOfNumbers(5,3);
         System.out.println("Your number: " + d);
     }
-    //Created a new method that takes two parameters at the input and calculates their sum.
+    //Created a new method that takes two parameters at the input and calculates their sum
     public static int sumOfNumbers (int a, int b){
         int c = a + b;
-        return (c);
+        return c;
     }
 }
